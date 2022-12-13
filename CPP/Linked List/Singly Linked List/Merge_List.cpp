38bb8781260c31764742c9c37b5cc62c70244be4
Gokul G.K <gokulgk1200@gmail.com>
2022-12-13 08:18:23 +0530

    Update Merge_List.cpp

--- CPP/Linked List/Singly Linked List/Merge_List.cpp
+++ CPP/Linked List/Singly Linked List/Merge_List.cpp
@@ -10,7 +10,10 @@
  */
 #include <iostream>
 using namespace std;
-
+/**
+ * @brief Node Template class
+ * 
+ */
 class Node
 {
 public:
@@ -25,6 +28,12 @@ public:
 
 Node *head1 = new Node(3);
 Node *head2 = new Node(9);
+/**
+ * @brief printLinkedList
+ * printing linked list
+ * 
+ * @param head 
+ */
 void printLinkedList(Node *head)
 {
     Node *temp = head;
@@ -35,7 +44,12 @@ void printLinkedList(Node *head)
     }
     cout << endl;
 }
-
+/**
+ * @brief addLast, adding element in last.
+ * 
+ * @param val 
+ * @param head 
+ */
 void addLast(int val, Node *head)
 {
     Node *temp = head;
@@ -46,8 +60,50 @@ void addLast(int val, Node *head)
     Node *n = new Node(val);
     temp->next = n;
 }
+/**
+ * @brief mergingLinkedList
+ * 
+ * Merging two sorted list ,
+ * space - O(n1+n2)
+ * time - O(n1+n2)
+ * 
+ * @param head1 
+ * @param head2 
+ * @return Node* 
+ */
+Node* mergingLinkedList(Node *head1, Node *head2){
+    Node *dummy = new Node(0);
+    Node *root = dummy;
+    //Iterate over these list and check these values and create new list
+    while(head1 != NULL && head2 != NULL){
+        if(head1->val > head2->val){
+            root->next = head2;
+            head2 = head2->next;
+        }else{
+            root->next = head1;
+            head1 = head1->next;          
+        }
+        root=root->next;
+
+    }
+    // Add remaining elements in the end.
+        while(head1 != NULL){
+            root->next = head1;
+            root=root->next;
+            head1 = head1->next; 
+        }
+
+        while(head2 != NULL){
+            root->next = head2;
+            root=root->next;
+            head2 = head2->next; 
+        }
+    
+    return dummy->next;
+}
 int main()
 {
+    //Driver code to run method.
     addLast(10, head1);
     addLast(20, head1);
     addLast(30, head1);
@@ -67,4 +123,8 @@ int main()
     printLinkedList(head1);
     cout << "Second list" << endl;
     printLinkedList(head2);
+    cout << "List after merging" << endl;
+    Node *root = mergingLinkedList(head1,head2);
+    printLinkedList(root);
+
 }
